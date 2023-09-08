e095a1bbbfb3e4301acaf4b30410f78ac16c5769
suprabhatkumar <suprabhatniraj@gmail.com>
2023-09-08 18:28:00 +0530

    make a simple server

---
+++ app.js
@@ -0,0 +1,18 @@
+const data=[
+    {
+        name:"niraj singh",
+        email:"niraj@gmail.com",
+        password:45465
+    },
+    {
+        name: "neha singh",
+        email: "neha@gmail.com",
+        password: 877646
+    },
+    {
+        name: "sonu singh",
+        email: "sonu@gmail.com",
+        password: 734745
+    }
+]
+module.exports=data;
--- index.js
+++ index.js
@@ -1,1 +1,7 @@
-console.log('hello jaan');
+const server =require('http');
+const data =require('./app')
+server.createServer((req,res)=>{
+    res.writeHead(200,{'content-type':'application\JSON'});
+    res.write(JSON.stringify (data))
+    res.end();
+}).listen(4500);
